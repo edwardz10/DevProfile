c3a4dfc959d49fd05586c5d0dbc51d90136a1c56
edward.z10 <edward.ziyangulov@gmail.com>
2021-01-03 20:00:44 +0300

    lesson 4

--- app/src/main/java/com/jonnyb/devprofile/MainActivity.kt
+++ app/src/main/java/com/jonnyb/devprofile/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jonnyb.devprofile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
     }
 }
